6c6dea7c9c6b6903d15580884162af737a19cdc5
Kulikov Nikita <nikitakulikov1444@gmail.com>
2022-02-18 09:08:02 +0900

    comment added

--- pick-a-card.rb
+++ pick-a-card.rb
@@ -2,3 +2,5 @@ values = ['2','3','4','5','6','7','8','9','10','J','q','K','A']
 suits = ['Diamonds','Hearts','Clubs','Spades']#Буби, Черви, Крести, Пики
 
 puts "#{values.sample} of #{suits.sample}"
+
+# comment
